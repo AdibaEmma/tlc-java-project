625edfe23a9932135f38d5dbcd3416ae7c083b60
Emmanuel Adiba <eabaagah@gmail.com>
2021-10-08 16:44:41 +0000

    create Lecture class and Test

---
+++ src/Lecture.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Lecture {
+
+    protected void enter(Student student) {
+
+    }
+
+    public double getHighestAverageGrade(List<Double> averageStudentGrades) {
+        double highestAverage = 0.00;
+        for (double averageGrade : averageStudentGrades) {
+            if (averageGrade > highestAverage) {
+                highestAverage = averageGrade;
+            }
+        }
+
+        return highestAverage;
+    }
+}
