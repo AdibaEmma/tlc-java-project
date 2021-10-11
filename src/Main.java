d2317bdca36e0e93d5032f8a01c63bbaeaa4a314
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2021-10-11 17:19:54 +0000

    update register class to return a list of of student based on level

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,41 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> students = new ArrayList<String>();
-        Student issac = new Student("Isaac", HasLevel.Level.LEVEL_200);
+        ArrayList<String> students = new ArrayList<String>();
+        Student isaac = new Student("Isaac", HasLevel.Level.LEVEL_200);
         Student emma = new Student("Emma", HasLevel.Level.LEVEL_100);
         Student jane = new Student("Jane", HasLevel.Level.LEVEL_200);
         Student michael = new Student("Michael", HasLevel.Level.LEVEL_100);
 
-        students.add(issac.name);
+//        System.out.println(emma.toString());
+
+        students.add(isaac.name);
         students.add(emma.name);
         students.add(jane.name);
         students.add(michael.name);
 
-        System.out.println(emma.toString());
+        // Create students list for a particular level
+        ArrayList<String> studentsAtLevel100 = new ArrayList<String>();
+        studentsAtLevel100.add(isaac.name);
+        studentsAtLevel100.add(jane.name);
+
+        ArrayList<String> studentsAtLevel200 = new ArrayList<String>();
+        studentsAtLevel200.add(emma.name);
+        studentsAtLevel200.add(michael.name);
+
+        Map<HasLevel.Level, ArrayList<String>> studentsWithLevel = new HashMap<>();
+        studentsWithLevel.put(HasLevel.Level.LEVEL_200, studentsAtLevel200);
+        studentsWithLevel.put(HasLevel.Level.LEVEL_100, studentsAtLevel100);
+
+
+//        System.out.println(emma.toString());
+
+
+
+        Register register = new Register(students);
+        Register registerWithLevels = new Register(studentsWithLevel);
+
+        registerWithLevels.getRegisterByLevel(HasLevel.Level.LEVEL_200);
     }
 }
--- src/Register.java
+++ src/Register.java
@@ -1,12 +1,15 @@
-import java.util.List;
-
+import java.util.*;
 
 public class Register {
 
     private List<String> students;
+    Map<HasLevel.Level, ArrayList<String>> studentsWithLevels;
 
     public Register(List<String>  students) {
         this.students = students;
+    }
+    public Register(Map<HasLevel.Level, ArrayList<String>> studentsWithLevels) {
+        this.studentsWithLevels = studentsWithLevels;
     }
 
 
@@ -14,4 +17,31 @@ public class Register {
         return students;
 }
 
+    public void getRegisterByLevel(HasLevel.Level level) {
+
+        switch (level) {
+            case LEVEL_100:
+                System.out.println(
+                        studentsWithLevels.get(HasLevel.Level.LEVEL_100)
+                );
+
+                break;
+            case LEVEL_200:
+                System.out.println(
+                        studentsWithLevels.get(HasLevel.Level.LEVEL_200)
+                );
+                break;
+            case LEVEL_300:
+                System.out.println(
+                        studentsWithLevels.get(HasLevel.Level.LEVEL_300)
+                );
+                break;
+            case LEVEL_400:
+                System.out.println(
+                        studentsWithLevels.get(HasLevel.Level.LEVEL_400)
+                );
+                break;
+            default: System.out.println("No students found for that level");
+        }
+    }
 }
